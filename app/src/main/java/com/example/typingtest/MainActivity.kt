45d9f2eaaeff90b69b24277a6f6c78168fa9cc05
Lou Xuan Dio <louxuan@bu.edu>
2025-02-11 16:10:59 -0500

    Complete WPM and the update function. Add references.

--- app/src/main/java/com/example/typingtest/MainActivity.kt
+++ app/src/main/java/com/example/typingtest/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -26,6 +27,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.typingtest.ui.theme.TypingTestTheme
+import kotlinx.coroutines.delay
 import org.xmlpull.v1.XmlPullParser
 
 class MainActivity : ComponentActivity() {
@@ -46,6 +48,28 @@ fun MainScreen(){
     var words = remember { loadWords(context).toMutableList() }
     var inputWord by remember { mutableStateOf("") }
 
+    var wordsCompleted by remember { mutableStateOf(0) }
+    var secondsFromStart by remember { mutableStateOf(0) }
+    var startTyping by remember { mutableStateOf(false) }
+    var wpm = if (secondsFromStart!=0) 1.0*wordsCompleted/secondsFromStart else 0.0
+
+    LaunchedEffect(startTyping) {
+        while (true){
+            delay(1000L)
+            secondsFromStart++
+        }
+    }
+
+    LaunchedEffect(Unit) {
+        while (true){
+            delay(5000L)
+            var newWords = loadWords(context)
+            if (newWords.isNotEmpty()){
+                words.clear()
+                words.addAll(newWords.shuffled())
+            }
+        }
+    }
     Row {
         LazyColumn (
             modifier = Modifier
@@ -65,17 +89,27 @@ fun MainScreen(){
                 .padding(16.dp)
                 .fillMaxHeight()
         ){
+            Text(text = "Your WPM: "+wpm,
+                modifier = Modifier.padding(4.dp),
+                fontSize = 20.sp)
+            //The TextField is written with the help of ChatGPT
             TextField(
                 value = inputWord,
                 onValueChange = {newWord ->
                     inputWord = newWord
-
+                    startTyping = true
                     if (words.contains(newWord)){
                         words.remove(newWord)
+                        wordsCompleted++
                         inputWord = ""
-                        //TODO: add a new one
+
+                        var newWords = loadWords(context)
+                            if (newWords.isNotEmpty()){
+                                words.add(newWords.random())     //by ChatGPT
+                            }
                     }
-                }
+                },
+                label = { Text("Type here") }
             )
         }
     }
